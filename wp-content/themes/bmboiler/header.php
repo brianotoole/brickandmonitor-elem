3d5dcc87c4a6b164ad25493d67632929d60db58a
brianotoole <botoole518@gmail.com>
2020-05-28 16:32:04 -0400

    elem specifics

--- wp-content/themes/bmboiler/header.php
+++ wp-content/themes/bmboiler/header.php
@@ -17,6 +17,12 @@
 
 <body <?php body_class(); ?>>
 
+<?php
+if ( ! function_exists( 'elementor_theme_do_location' ) || ! elementor_theme_do_location( 'header' ) ) {
+	get_template_part( 'template-parts/header' );
+}
+?>
+
 <!-- LOADING -->
 <div class="loader-wrapper" id="loader">
   <div class="team-loader">
